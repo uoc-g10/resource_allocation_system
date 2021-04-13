1c578d4aabcb2701486166c04fceb3973f0b4d3c
unknown <2019t00468@stu.cmb.ac.lk>
2021-04-13 12:14:19 +0530

    First Commit

--- resource_module/department_create.php
+++ resource_module/department_create.php
@@ -1,1 +1,25 @@
-sss
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "allocationsystem";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $_POST['name'];
+$sql = "INSERT INTO departments SET name='$name'";
+
+if($conn->query($sql)==TRUE){
+  echo "New Department has been created successfully";
+} else{
+  echo "Error".$sql.$conn->error;
+}
+
+$conn->close();
+
+?>
--- resource_module/resource_create.php
+++ resource_module/resource_create.php
@@ -1,1 +1,30 @@
-sss
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "allocationsystem";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $_POST['name'];
+$location = $_POST['location'];
+$type = $_POST['type'];
+$department = $_POST['department'];
+$cat = $_POST['catogory'];
+
+$sql = "INSERT INTO resources (name,location,type,department,category) VALUES('$name','$location','$type','$department','$cat')";
+
+
+if($conn->query($sql)==TRUE){
+  echo "New Department has been created successfully";
+} else{
+  echo "Error".$sql.$conn->error;
+}
+
+$conn->close();
+?>
